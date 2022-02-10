94a27f3d529e6570ea5f69c46b098170de2ce4db
Yatogamikaka <l504922010@hotmail.com>
2022-02-10 15:33:14 +0800

    add echarts.js

--- js/animate.js
+++ js/animate.js
@@ -17,6 +17,18 @@ $(function () {
         //     $(this).parents('tr').stop().fadeIn();
         // }
     });
+    $('tbody').on('change', 'input', function () {
+        var cName = $(this).parents('tbody').prop('class');
+        // console.log(cName);
+        var index = $(this).parents('tr').index()
+        // console.log(index);
+        var data = getLocalStorage(cName);
+        // console.log(data);
+        data[index].done = $(this).prop('checked');
+        // console.log(data[index]);
+        saveLocalStorage(cName, data);
+        updataStatueData(myStackCharts);
+    });
     var flagCounter = {
         // 数值代表第几次点击 
         // 第一次隐藏已完成的项目 显示未完成的项目
@@ -26,7 +38,7 @@ $(function () {
         cvH: 0,
         bbBF: 0,
         bbH: 0
-    }
+    };
     $('thead').on('click', '.switch', function () {
         // console.log($(this).prop('id'));
         var key = $(this).prop('id');
@@ -49,4 +61,115 @@ $(function () {
             flagCounter[key] = 0;
         }
     });
-})
+
+    // echarts 图表
+    // 点击事件
+    $('#displatC').on('click', function () {
+        $('.mask').stop().show();
+        $('.echarts').stop().show();
+    });
+    $('.mask,.echarts a').on('click', function () {
+        $('.mask').stop().hide();
+        $('.echarts').stop().hide();
+    });
+    var myStackCharts = echarts.init(document.querySelector('.barChart'));
+    var myStackOption = {
+        title: {
+            text: '舰船养成数量'
+        },
+        tooltip: {},
+        xAxis: {
+            type: 'category',
+            data: ['航母装填', '航母命中', '战列装填', '战列命中'],
+        },
+        yAxis: {},
+        legend: {
+            data: ['已完成', '未完成'],
+        },
+        grid: {
+            left: '5%',
+            right: '5%',
+            buttom: '0%',
+        },
+        series: [
+            {
+                name: '已完成',
+                type: 'bar',
+                data: [],
+                stack: 'x',
+                itemStyle: {
+                    normal: {
+                        label: {
+                            show: true,
+                            position: 'right',
+                            textStyle: {
+                                color: 'black',
+                                fontSize: 13
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                name: '未完成',
+                type: 'bar',
+                data: [],
+                stack: 'x',
+                itemStyle: {
+                    normal: {
+                        label: {
+                            show: true,
+                            position: 'right',
+                            textStyle: {
+                                color: 'black',
+                                fontSize: 13
+                            }
+                        }
+                    }
+                }
+            },
+        ],
+    };
+    myStackCharts.setOption(myStackOption);
+
+    // setOption的需要的对象
+    $('.cbox:first').change();
+});
+// 写一个函数更新已完成和未完成的数量，每次 input:checked 状态更新都调用这个函数，并重新 setOption
+function updataStatueData(echartsObj) {
+    var keys = ['cvBackFill', 'cvHit', 'bbBackFill', 'bbHit'];
+    var done = [];
+    var doing = [];
+    var count = 0;
+    for (var i = 0; i < keys.length; i++) {
+        var temp = getLocalStorage(keys[i]);
+        for (var _ in temp) {
+            // 获取过来的对象格式如下：
+            // {
+            //     'id': 1,
+            //     'done': true / false
+            // }
+            // done 属性表面是否已完成，这里只需要 done 属性
+            if (temp[_]['done']) {
+                count++;
+            }
+            // console.log(_);
+            // console.log(temp[_]['done']);
+        }
+        done.push(count);
+        doing.push(temp.length - count);
+        count = 0;
+    }
+    echartsObj.setOption({
+        series: [
+            {
+                name: '已完成',
+                data: done,
+            },
+            {
+                name: '未完成',
+                data: doing,
+            }
+        ]
+    });
+}
--- js/storage.js
+++ js/storage.js
@@ -1,63 +1,53 @@
 $(function () {
     genJson();
     loadLocalStorage();
-    $('tbody').on('change', 'input', function () {
-        var cName = $(this).parents('tbody').prop('class');
-        // console.log(cName);
-        var index = $(this).parents('tr').index()
-        // console.log(index);
-        var data = getLocalStorage(cName);
+});
+
+
+function getLocalStorage(name) {
+    var data = localStorage.getItem(name);
+    if (data) {
+        data = JSON.parse(data);
+        return data;
+    } else {
+        return null;
+    }
+}
+function saveLocalStorage(name, data) {
+    localStorage.setItem(name, JSON.stringify(data));
+}
+function loadLocalStorage() {
+    var search = ['cvBackFill', 'cvHit', 'bbBackFill', 'bbHit'];
+    // $('.' + search[i]).find('tr').eq(data[j].id).find('input').prop('checked', data[j].done);
+    for (var i = 0; i < search.length; i++) {
+        var data = getLocalStorage(search[i]);
+        for (var j = 0; j < data.length; j++) {
+            // console.log(search[i] + ':-> id:' + data[j].id + '-> done:' + data[j].done);
+            $('.' + search[i]).find('tr').eq(data[j].id).find('input').prop('checked', data[j].done);
+            // console.log($('.' + search[i]).find('tr').eq(data[j].id).find('input').prop('checked'));
+            // console.log('data[j].id' + data[j].id);
+        }
+    }
+}
+function genJson() {
+    // 第一次加载将生成一个全空的本地存储
+    var data = getLocalStorage('cvBackFill');
+    if (data != null) {
+        return null;
+    }
+    $.each($('tbody'), function (i, ele) {
+        // console.log($(ele).prop('class').substr(6));
+        var data = [];
+        // console.log($(ele).find('tr'));
         // console.log(data);
-        data[index].done = $(this).prop('checked');
-        // console.log(data[index]);
-        saveLocalStorage(cName, data);
-    });
-
-    function getLocalStorage(name) {
-        var data = localStorage.getItem(name);
-        if (data) {
-            data = JSON.parse(data);
-            return data;
-        } else {
-            return null;
-        }
-    }
-    function saveLocalStorage(name, data) {
-        localStorage.setItem(name, JSON.stringify(data));
-    }
-    function loadLocalStorage() {
-        var search = ['cvBackFill', 'cvHit', 'bbBackFill', 'bbHit'];
-        // $('.' + search[i]).find('tr').eq(data[j].id).find('input').prop('checked', data[j].done);
-        for (var i = 0; i < search.length; i++) {
-            var data = getLocalStorage(search[i]);
-            for (var j = 0; j < data.length; j++) {
-                // console.log(search[i] + ':-> id:' + data[j].id + '-> done:' + data[j].done);
-                $('.' + search[i]).find('tr').eq(data[j].id).find('input').prop('checked', data[j].done);
-                // console.log($('.' + search[i]).find('tr').eq(data[j].id).find('input').prop('checked'));
-                // console.log('data[j].id' + data[j].id);
-            }
-        }
-
-    }
-    function genJson() {
-        var data = getLocalStorage('cvBackFill');
-        if (data != null) {
-            return null;
-        }
-        $.each($('tbody'), function (i, ele) {
-            // console.log($(ele).prop('class').substr(6));
-            var data = [];
-            // console.log($(ele).find('tr'));
-            // console.log(data);
-            $.each($(ele).find('tr'), function (i, e) {
-                // console.log('id:' + $(e).index());
-                // console.log('done:' + $(e).find('input').prop('checked'));
-                data.push({
-                    id: $(e).index(),
-                    done: $(e).find('input').prop('checked')
-                });
-            })
-            saveLocalStorage($(ele).prop('class'), data);
+        $.each($(ele).find('tr'), function (i, e) {
+            // console.log('id:' + $(e).index());
+            // console.log('done:' + $(e).find('input').prop('checked'));
+            data.push({
+                id: $(e).index(),
+                done: $(e).find('input').prop('checked')
+            });
         })
-    }
-})
+        saveLocalStorage($(ele).prop('class'), data);
+    })
+}
